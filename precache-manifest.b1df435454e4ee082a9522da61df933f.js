5f86ad77b5f672fb6154c10997e31001076b8f00
= <=>
2019-11-19 09:41:07 +0700

    UGD

---
+++ precache-manifest.b1df435454e4ee082a9522da61df933f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5e9115a2655080129e",
+    "url": "/css/chunk-vendors.48fa860f.css"
+  },
+  {
+    "revision": "2796a2c0d592a66efbe9333df4c72f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d8c331ee05abec0240",
+    "url": "/js/about.1e298b00.js"
+  },
+  {
+    "revision": "da17c9f3b25ff4638693",
+    "url": "/js/app.be682f45.js"
+  },
+  {
+    "revision": "dd5e9115a2655080129e",
+    "url": "/js/chunk-vendors.82f193d3.js"
+  },
+  {
+    "revision": "c99b971e26a19a7befb6d43aa8fb25a3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
